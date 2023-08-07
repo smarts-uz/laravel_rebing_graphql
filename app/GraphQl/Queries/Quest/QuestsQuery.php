990c380c013ba0758aa407fd525011dfa8a302eb
User <elorgray@gmail.com>
2023-08-07 15:27:18 +0500

    init category and quests query

---
+++ app/GraphQl/Queries/Quest/QuestsQuery.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\GraphQl\Queries\Quest;
+
+use App\Models\Quest;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Query;
+class QuestsQuery extends Query
+{
+    protected $attributes = [
+        'name' => 'quests',
+    ];
+
+    public function type(): Type
+    {
+        return Type::listOf(GraphQL::type('Quest'));
+    }
+
+    public function resolve($root, $args)
+    {
+        return Quest::all();
+    }
+}
