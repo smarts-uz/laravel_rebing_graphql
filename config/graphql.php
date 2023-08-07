83d01bbb9b95dea51e0794858d37f1d0f76f8d81
User <elorgray@gmail.com>
2023-08-07 15:41:43 +0500

    change config graphql.cfg

--- config/graphql.php
+++ config/graphql.php
@@ -75,25 +75,26 @@ return [
     'schemas' => [
         'default' => [
             'query' => [
-                // ExampleQuery::class,
+                'quest' => \App\GraphQL\Queries\Quest\QuestQuery::class,
+                'quests' => \App\GraphQL\Queries\Quest\QuestsQuery::class,
+                'category' => \App\GraphQL\Queries\Category\CategoryQuery::class,
+                'categories' => \App\GraphQL\Queries\Category\CategoriesQuery::class,
             ],
             'mutation' => [
-                // ExampleMutation::class,
+    'createQuest' => \App\GraphQL\Mutations\Quest\CreateQuestMutation::class,
+                'updateQuest' => \App\GraphQL\Mutations\Quest\UpdateQuestMutation::class,
+                'deleteQuest' => \App\GraphQL\Mutations\Quest\DeleteQuestMutation::class,
+                'createCategory' => \App\GraphQL\Mutations\Category\CreateCategoryMutation::class,
+                'updateCategory' => \App\GraphQL\Mutations\Category\UpdateCategoryMutation::class,
+                'deleteCategory' => \App\GraphQL\Mutations\Category\DeleteCategoryMutation::class,
             ],
-            // The types only available in this schema
-            'types' => [
-                // ExampleType::class,
-            ],
-
-            // Laravel HTTP middleware
-            'middleware' => null,
-
-            // Which HTTP methods to support; must be given in UPPERCASE!
-            'method' => ['GET', 'POST'],
-
-            // An array of middlewares, overrides the global ones
-            'execution_middleware' => null,
+            'middleware' => [],
+            'method' => ['get', 'post'],
         ],
+    ],
+		'types' => [
+    'Quest' => \App\GraphQL\Types\QuestType::class,
+       'Category' => \App\GraphQL\Types\CategoryType::class
     ],
 
     // The global types available to all schemas.
@@ -105,11 +106,6 @@ return [
     //     App\GraphQL\Types\UserType::class
     // ]
     //
-    'types' => [
-        // ExampleType::class,
-        // ExampleRelationType::class,
-        // \Rebing\GraphQL\Support\UploadType::class,
-    ],
 
     // This callable will be passed the Error object for each errors GraphQL catch.
     // The method should return an array representing the error.
