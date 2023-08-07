31c2b846613b4a8ab39e9f93d05460f9104e746c
User <elorgray@gmail.com>
2023-08-07 15:19:33 +0500

    init category and quests types

---
+++ app/GraphQl/Types/CategoryType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\GraphQl\Types;
+
+use App\Models\Category;
+use GraphQL\Type\Definition\Type;
+use Rebing\GraphQL\Support\Facades\GraphQL;
+use Rebing\GraphQL\Support\Type as GraphQLType;
+class CategoryType extends GraphQlType
+{
+
+    protected $attributes = [
+        'name' => 'Category',
+        'description' => 'Collection of categories',
+        'model' => Category::class
+    ];
+    public function fields(): array
+    {
+        return [
+            'id' => [
+                'type' => Type::nonNull(Type::int()),
+                'description' => 'ID of quest'
+            ],
+            'title' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'Title of the quest'
+            ],
+            'quests' => [
+                'type' => Type::listOf(GraphQL::type('Quest')),
+                'description' => 'List of quests'
+            ]
+        ];
+    }
+}
